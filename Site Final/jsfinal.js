ff23b8a7899fedb25dec63934f7bcaaada7ac732
fantineF <81324947+fantineF@users.noreply.github.com>
2021-07-02 09:39:36 -0300

    Add files via upload

---
+++ Site Final/jsfinal.js
@@ -0,0 +1,14 @@
+let films; 
+films = prompt( 'Voce gosta de Séries e Filmes?');  
+     
+
+if (films === 'sim'){
+    alert('Está no lugar certo, temos varios para te indicar!!');  
+
+} 
+else if(films=== 'Sim'){
+    alert('Está no lugar certo, temos varios para te indicar!!'); 
+}
+else {
+    alert('Ahh que pena!! mas deixa eu te mostrar alguns bem legais?'); 
+} 
